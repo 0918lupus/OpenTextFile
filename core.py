377beb43c738122f06548acd5471e44c32d89762
kitamura takumi <0918lobo@gmail.com>
2022-11-19 00:41:33 +0900

    aaa

--- core.py
+++ core.py
@@ -4,9 +4,8 @@ import bpy
 
 from .register_class import _get_cls
 
-
-class COU_OT_open_url(bpy.types.Operator):
-    bl_idname = "object.open_url"
+class COT_OT_open_text_file(bpy.types.Operator):
+    bl_idname = "object.open_text_file"
     bl_label = "Open URL"
     bl_description = "Open the URL of a text object."
 
@@ -18,25 +17,6 @@ class COU_OT_open_url(bpy.types.Operator):
         if lst:
             webbrowser.open(lst[0].data.body)
         return {"FINISHED"}
-
-
-class COU_OT_add_url(bpy.types.Operator):
-    bl_idname = "object.add_url"
-    bl_label = "Add URL"
-    bl_description = "Add the a text object of URL."
-
-    def execute(self, context):
-        s = bpy.context.window_manager.clipboard
-        if not (isinstance(s, str) and s.startswith("http")):
-            self.report({"WARNING"}, "Copy URL")
-            return {"CANCELLED"}
-        bpy.ops.object.text_add(radius=0.1)
-        text = bpy.context.object
-        text.data.body = s
-        text.name = "URL"
-        text.hide_render = True
-        return {"FINISHED"}
-
 
 # 自動的にこのモジュールのクラスを設定
 ui_classes = _get_cls(__name__)
@@ -50,9 +30,9 @@ def draw_item(self, context):
 
 def register():
     """追加登録用（クラス登録は、register_class内で実行）"""
-    bpy.types.VIEW3D_MT_object.append(draw_item)
+    bpy.types.TEXT_MT_text.append(draw_item)
 
 
 def unregister():
     """追加削除用（クラス削除は、register_class内で実行）"""
-    bpy.types.VIEW3D_MT_object.remove(draw_item)
+    bpy.types.TEXT_MT_text.remove(draw_item)
